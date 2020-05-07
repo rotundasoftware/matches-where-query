39a2e13fde9a3f110a5d4ae30a1200406c7c5cdd
martinflory <martin@rotundasoftware.com>
2020-05-07 14:52:23 -0300

    added startsWith and endsWith (#6)
    * :hammer: added startsWith and endsWith
    
    * :hammer: removed position in the helper functions
    
    * :hammer: removed lib es6 version
    
    * cleanup test
    
    * removing eslint for js, needs more work
    
    * Circleci project setup (#7)
    
    * Add .circleci/config.yml
    
    * install only prod
    
    * :ok_hand: fixes based on PR
    
    - Removed eslint module and dep
    - Removed `--production` label from npm install in the circleCI config
    - Added validation for null objectAttribute for string comparisons
    
    * :hammer: testing, testing, testing
    
    * :hammer: removing unneeded string check
    
    Co-authored-by: Martin Flory <martin.flory@rotundasoftware.com>
    Co-authored-by: Javier Viola <pepoviola@gmail.com>

---
+++ tests/test.js
@@ -0,0 +1,208 @@
+var _ = require( 'underscore' );
+var chai = require( 'chai' );
+var matchesWhereQuery = require( '../matchesWhereQuery' );
+var expect = chai.expect;
+
+describe( 'Matches where query Test', function() {
+	var person;
+
+	beforeEach( function() {
+		person = {
+			firstName : 'Martin',
+			lastName : 'Flores',
+			height : 172,
+			city : 'Buenos Aires' 
+		};
+	} );
+
+	describe( 'Equals (single)', function() {
+		it( 'Match', function() {
+			var comparator = { firstName : 'Martin' };
+	
+			expect( matchesWhereQuery( person, comparator ) ).to.be.true;
+		} );
+
+		it( 'Not Match', function() {
+			var comparator = { firstName : 'Marton' };
+			
+			expect( matchesWhereQuery( person, comparator ) ).to.be.false;
+		} );
+	} );
+
+	describe( 'Equals (array)', function() {
+		it( 'Match', function() {
+			var comparator = { firstName : [ 'John', 'Peter', 'Martin' ] };
+	
+			expect( matchesWhereQuery( person, comparator ) ).to.be.true;
+		} );
+
+		it( 'Not Match', function() {
+			var comparator = { firstName : [ 'John', 'Peter', 'Sarah' ] };
+
+			expect( matchesWhereQuery( person, comparator ) ).to.be.false;
+		} );
+	} );
+
+	describe( 'doesNotEqual', function() {
+		it( 'Match', function() {
+			var comparator = { firstName : { comparator : 'doesNotEqual', value : 'Jonh' } };
+	
+			expect( matchesWhereQuery( person, comparator ) ).to.be.true;
+		} );
+
+		it( 'Not Match', function() {
+			var comparator = { firstName : { comparator : 'doesNotEqual', value : 'Martin' } };
+
+			expect( matchesWhereQuery( person, comparator ) ).to.be.false;
+		} );
+	} );
+
+	describe( 'isGreaterThan', function() {
+		it( 'Match', function() {
+			var comparator = { height : { comparator : 'isGreaterThan', value : 170 } };
+	
+			expect( matchesWhereQuery( person, comparator ) ).to.be.true;
+		} );
+
+		it( 'Not Match', function() {
+			var comparator = { height : { comparator : 'isGreaterThan', value : 180 } };
+
+			expect( matchesWhereQuery( person, comparator ) ).to.be.false;
+		} );
+	} );
+
+	describe( 'isGreaterThanOrEqualTo', function() {
+		it( 'Match', function() {
+			var comparator = { height : { comparator : 'isGreaterThanOrEqualTo', value : 172 } };
+	
+			expect( matchesWhereQuery( person, comparator ) ).to.be.true;
+		} );
+
+		it( 'Not Match', function() {
+			var comparator = { height : { comparator : 'isGreaterThanOrEqualTo', value : 173 } };
+			expect( matchesWhereQuery( person, comparator ) ).to.be.false;
+		} );
+	} );
+
+	describe( 'isLessThan', function() {
+		it( 'Match', function() {
+			var comparator = { height : { comparator : 'isLessThan', value : 174 } };
+	
+			expect( matchesWhereQuery( person, comparator ) ).to.be.true;
+		} );
+
+		it( 'Not Match', function() {
+			var comparator = { height : { comparator : 'isLessThan', value : 172 } };
+			expect( matchesWhereQuery( person, comparator ) ).to.be.false;
+		} );
+	} );
+
+	describe( 'isLessThanOrEqualTo', function() {
+		it( 'Match', function() {
+			var comparator = { height : { comparator : 'isLessThanOrEqualTo', value : 172 } };
+	
+			expect( matchesWhereQuery( person, comparator ) ).to.be.true;
+		} );
+
+		it( 'Not Match', function() {
+			var comparator = { height : { comparator : 'isLessThanOrEqualTo', value : 171 } };
+			expect( matchesWhereQuery( person, comparator ) ).to.be.false;
+		} );
+	} );
+
+	describe( 'isBetween', function() {
+		it( 'Match', function() {
+			var comparator = { height : { comparator : 'isBetween', value : [ 171, 176 ] } };
+	
+			expect( matchesWhereQuery( person, comparator ) ).to.be.true;
+		} );
+
+		it( 'Not Match - correct array', function() {
+			var comparator = { height : { comparator : 'isBetween', value : [ 120, 140 ] } };
+			expect( matchesWhereQuery( person, comparator ) ).to.be.false;
+		} );
+
+		it( 'Not Match - incorrect comparator values', function() {
+			expect( () => matchesWhereQuery( person, { height : { comparator : 'isBetween', value : 2 } } ) ).to.throw();
+			expect( () => matchesWhereQuery( person, { height : { comparator : 'isBetween', value : [ 171, 142, 176 ] } } ) ).to.throw();
+			expect( matchesWhereQuery( person, { height : { comparator : 'isBetween', value : [ 176, 140 ] } } ) ).to.be.false;
+		} );
+	} );
+
+	describe( 'startsWith', function() {
+		it( 'Simple Match', function() {
+			var comparator = { firstName : { comparator : 'startsWith', value : 'Mar' } };
+	
+			expect( matchesWhereQuery( person, comparator ) ).to.be.true;
+		} );
+
+		it( 'Simple Not Match', function() {
+			var comparator = { firstName : { comparator : 'startsWith', value : 'Mer' } };
+			expect( matchesWhereQuery( person, comparator ) ).to.be.false;
+		} );
+
+		it( 'Null Not Match', function() {
+			person.firstName = null;
+			var comparator = { firstName : { comparator : 'startsWith', value : 'Mer' } };
+			expect( matchesWhereQuery( person, comparator ) ).to.be.false;
+		} );
+
+		it( 'No a string for startsWith comparator - throws', function() {
+			var comparator = { firstName : { comparator : 'startsWith', value : 3 } };
+
+			expect( () => matchesWhereQuery( person, comparator ) ).to.throw();
+		} );
+	} );
+
+	describe( 'endsWith', function() {
+		it( 'Simple Match', function() {
+			var comparator = { firstName : { comparator : 'endsWith', value : 'tin' } };
+			expect( matchesWhereQuery( person, comparator ) ).to.be.true;
+		} );
+
+		it( 'Simple Not Match', function() {
+			var comparator = { firstName : { comparator : 'endsWith', value : 'nit' } };
+			expect( matchesWhereQuery( person, comparator ) ).to.be.false;
+		} );
+
+		it( 'Null Not Match', function() {
+			person.firstName = null;
+			var comparator = { firstName : { comparator : 'startsWith', value : 'Mer' } };
+			expect( matchesWhereQuery( person, comparator ) ).to.be.false;
+		} );
+
+		it( 'No a string for startsWith comparator - throws', function() {
+			var comparator = { firstName : { comparator : 'startsWith', value : 3 } };
+
+			expect( () => matchesWhereQuery( person, comparator ) ).to.throw();
+		} );
+	} );
+
+	describe( 'Multi-clause query', function() {
+		it( 'Match', function() {
+			var comparator = {
+				firstName : { comparator : 'endsWith', value : 'tin' },
+				height : { comparator : 'isGreaterThan', value : 171 }
+			};
+
+			expect( matchesWhereQuery( person, comparator ) ).to.be.true;
+		} );
+
+		it( 'Not match', function() {
+			var comparator = {
+				firstName : { comparator : 'endsWith', value : 'tin' },
+				height : { comparator : 'isLessThanOrEqualTo', value : 171 }
+			};
+
+			expect( matchesWhereQuery( person, comparator ) ).to.be.false;
+		} );
+	} );
+
+	describe( 'Misc', function() {
+		it( 'No value for comparator queries throws', function() {
+			var comparator = { firstName : { comparator : 'endsWith' } };
+
+			expect( () => matchesWhereQuery( person, comparator ) ).to.throw();
+		} );
+	} );
+} );
